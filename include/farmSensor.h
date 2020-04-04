ef560f60b9a1a6129e212ee74b4b369947916ed7
tarek20501 <39224871+tarek20501@users.noreply.github.com>
2020-04-04 12:15:14 -0700

    Sensors Class Revised (NOT TESTED)

--- include/farmSensor.h
+++ include/farmSensor.h
@@ -6,8 +6,9 @@
 
 #pragma once
 #include <Arduino.h>
+#include "LoRaTransceiver.h"
 
-enum sensorType { digital, analog, soft };
+enum sensorType { digital, analog, counter };
 
 //template <class T>
 class farmSensor 
@@ -17,21 +18,24 @@ private:
     String m_name;
     String m_unit;
     sensorType m_type;
+    char m_shortcut;
     int m_data;
     int readRaw();
 public:
-    farmSensor(int pin, sensorType type, String name, String unit);
+    farmSensor(int pin, sensorType type, String name, String unit, char shortcut);
     void initialize();
-    String read(int *p_data);
+    String read();
+    packet pack();
 };
 
 //template <class T>
-farmSensor::farmSensor(int pin, sensorType type, String name, String unit)
+farmSensor::farmSensor(int pin, sensorType type, String name, String unit, char shortcut)
 {
     m_pin = pin;
     m_type = type;
     m_name = name;
     m_unit = unit;
+    m_shortcut = shortcut;
     m_data = -1;
 }
 
@@ -53,7 +57,7 @@ int farmSensor::readRaw()
     case analog:
         return analogRead(m_pin);
         break;
-    case soft:
+    case counter:
         return m_data++; 
         break;
     default:
@@ -62,11 +66,18 @@ int farmSensor::readRaw()
     }
 }
 
-String farmSensor::read(int *p_data)
+String farmSensor::read()
 {
-    int raw = readRaw();
-    *p_data = raw;
-    return m_name + String(": ") + String(raw) + String(" (") + m_unit + String(")") + String(" | ");
+    m_data = readRaw();
+    return m_name + String(": ") + String(m_data) + String(" (") + m_unit + String(")") + String(" | ");
+}
+
+packet farmSensor::pack()
+{
+    packet ret;
+    ret.type = m_shortcut;
+    ret.data = m_data;
+    return ret;
 }
 
 // Sensors to be used:
--- src/main.cpp
+++ src/main.cpp
@@ -30,11 +30,6 @@ HSPI MOSI *|D13             D15|* HSPI CS
 #include "LoRaTransceiver.h"
 
 //Global Variables
-int counterData1;
-int counterData2;
-int counterData3;
-int counterData4;
-// Sensors data holders go here.
 String message;
 packet packets[NUMBER_OF_PACKETS];
 
@@ -42,10 +37,10 @@ packet packets[NUMBER_OF_PACKETS];
 watch rtc(false);
 SdCard memory;
 LoRaTransceiver receiver(15, 27, 26, 0xF3);
-farmSensor counter1(0, soft, "Counter1", "T");
-farmSensor counter2(0, soft, "Counter2", "T");
-farmSensor counter3(0, soft, "Counter3", "T");
-farmSensor counter4(0, soft, "Counter4", "T");
+farmSensor counter1(0, counter, "Counter1", "T", 'c');
+farmSensor counter2(0, counter, "Counter2", "T", 'd');
+farmSensor counter3(0, counter, "Counter3", "T", 'e');
+farmSensor counter4(0, counter, "Counter4", "T", 'f');
 //temp thermocouple(4);
 //Sensors Constructors go here.
 
@@ -80,10 +75,10 @@ void loop()
 {
   //Sampling Sensors
   digitalWrite(BUILTIN_LED, HIGH);
-  message  = counter1.read(&counterData1);
-  message += counter2.read(&counterData2);
-  message += counter3.read(&counterData3);
-  message += counter4.read(&counterData4);
+  message  = counter1.read();
+  message += counter2.read();
+  message += counter3.read();
+  message += counter4.read();
   message += rtc.getTimeStamp() + String("\n");
   
   //Writing on Sd Card
@@ -92,10 +87,10 @@ void loop()
   digitalWrite(BUILTIN_LED, LOW);
   
   //Waiting for a request on LoRa
-  packets[0].type = 'c'; packets[0].data = counterData1;
-  packets[1].type = 'd'; packets[1].data = counterData2;
-  packets[2].type = 'e'; packets[2].data = counterData3;
-  packets[3].type = 'f'; packets[3].data = counterData4;
+  packets[0] = counter1.pack();
+  packets[1] = counter2.pack();
+  packets[2] = counter3.pack();
+  packets[3] = counter4.pack();
   receiver.respond(packets, NUMBER_OF_PACKETS);
   
   Serial.println();
