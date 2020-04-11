53469d252d347c82c1ed1fcf45b0c2c106867071
Timothy <Timothy@DESKTOP-PJVQ42V.local>
2020-04-10 21:19:27 -0700

    My changes to temperature.h

--- include/temp.h
+++ include/temp.h
@@ -7,6 +7,7 @@
 #pragma once
 #include <OneWire.h> 
 #include <DallasTemperature.h>
+#include "LoRaTransceiver.h"
 
 class temp
 {
@@ -14,10 +15,15 @@ private:
     int m_pin;
     OneWire m_oneWire;
     DallasTemperature m_sensors;
+    int m_data;
+    float readRaw();
+
 public:
     temp(int pin);
     void initialize();
-    float readRaw();
+    packet pack(); //for sending packets
+    String read();
+
 };
 
 temp::temp(int pin): m_oneWire(pin), m_sensors(&m_oneWire)
@@ -33,4 +39,18 @@ float temp::readRaw()
 {
     m_sensors.requestTemperatures();
     return m_sensors.getTempCByIndex(0);
+}
+
+packet temp::pack()
+{
+    packet ret;
+    ret.type = 't';
+    ret.data = m_data;
+    return ret;
+}
+
+String temp::read()
+{
+    m_data = readRaw(); //temperature read
+    return String("Temperature") + String(": ") + String(m_data) + String(" (") + String("degrees Celsius") + String(")") + String(" | ");
 }
