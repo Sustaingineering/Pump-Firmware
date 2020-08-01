9a9799c968ff4081c6b6852e616fb97ee15ae5e1
Timothy <Timothy@DESKTOP-PJVQ42V.local>
2020-08-01 10:00:20 -0700

    Update temp.h

--- include/temp.h
+++ include/temp.h
@@ -8,8 +8,56 @@
 #include <OneWire.h> 
 #include <DallasTemperature.h>
 #include "LoRaTransceiver.h"
+#include "farmSensor.h"
+
+class temp: public farmSensor{
+
+    private:
+        OneWire m_oneWire;
+        DallasTemperature m_sensors; 
+    protected:
+        float readRaw();
+    public: 
+        temp(int pin, sensorType type, String name, String unit, char shortcut);
+        void initialize();
+        packet pack();
+        String read();
+
+};
+
+temp::temp(int pin, sensorType type, String name, String unit, char shortcut): 
+                farmSensor(pin, type, name, unit, shortcut), m_oneWire(pin), m_sensors(&m_oneWire) {}
 
 
+void temp::initialize()
+{
+    m_sensors.begin();
+}
+
+float temp::readRaw()
+{
+    m_sensors.requestTemperatures();
+    return m_sensors.getTempCByIndex(0);
+}
+
+packet temp::pack()
+{
+    packet ret;
+    ret.type = 't';
+    ret.data = m_data;
+    return ret;
+}
+
+String temp::read()
+{
+    m_data = readRaw(); //temperature read
+    return String("Temperature") + String(": ") + String(m_data) + String(" (") + String("degrees Celsius") + String(")") + String(" | ");
+}
+
+
+/* ------------------------- Original Code By Tarek ------------------------- */
+
+/*
 class temp
 {
 private:
@@ -55,3 +103,4 @@ String temp::read()
     m_data = readRaw(); //temperature read
     return String("Temperature") + String(": ") + String(m_data) + String(" (") + String("degrees Celsius") + String(")") + String(" | ");
 }
+*/
