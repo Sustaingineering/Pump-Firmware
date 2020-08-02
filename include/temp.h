4dfc14b60730c822eb1ac50d74609bc97a1639cb
Timothy <Timothy@DESKTOP-PJVQ42V.local>
2020-08-02 09:32:48 -0700

    Update temp.h

--- include/temp.h
+++ include/temp.h
@@ -11,16 +11,14 @@
 #include "farmSensor.h"
 
 class temp: public farmSensor{
-
-    private:
-        OneWire m_oneWire;
-        DallasTemperature m_sensors; 
-    protected:
-        float readRaw() override;
-    public: 
-        temp(int pin, sensorType type, String name, String unit, char shortcut);
-        void initialize() override;
-
+private:
+    OneWire m_oneWire;
+    DallasTemperature m_sensors; 
+protected:
+    float readRaw() override;
+public: 
+    temp(int pin, sensorType type, String name, String unit, char shortcut);
+    void initialize() override;
 };
 
 temp::temp(int pin, sensorType type, String name, String unit, char shortcut): 
