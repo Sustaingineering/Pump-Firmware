7b0c4c50e28fd113fa2a25aba208caae331ea2f1
tarek20501 <39224871+tarek20501@users.noreply.github.com>
2020-08-15 12:17:47 -0700

    Update temp.h

--- include/temp.h
+++ include/temp.h
@@ -7,28 +7,23 @@
 #pragma once
 #include <OneWire.h> 
 #include <DallasTemperature.h>
+#include "LoRaTransceiver.h"
+#include "farmSensor.h"
 
-
-class temp
-{
+class temp: public farmSensor{
 private:
-    int m_pin;
     OneWire m_oneWire;
-    DallasTemperature m_sensors;
-    int m_data;
-    float readRaw();
-
-public:
-    temp(int pin);
-    void initialize();
-    packet pack(); //for sending packets
-    String read();
-
+    DallasTemperature m_sensors; 
+protected:
+    float readRaw() override;
+public: 
+    temp(int pin, sensorType type, String name, String unit, char shortcut);
+    void initialize() override;
 };
 
-temp::temp(int pin): m_oneWire(pin), m_sensors(&m_oneWire)
-{
-}
+temp::temp(int pin, sensorType type, String name, String unit, char shortcut): 
+                farmSensor(pin, type, name, unit, shortcut), m_oneWire(pin), m_sensors(&m_oneWire) {}
+
 
 void temp::initialize()
 {
@@ -41,16 +36,3 @@ float temp::readRaw()
     return m_sensors.getTempCByIndex(0);
 }
 
-packet temp::pack()
-{
-    packet ret;
-    ret.type = 't';
-    ret.data = m_data;
-    return ret;
-}
-
-String temp::read()
-{
-    m_data = readRaw(); //temperature read
-    return String("Temperature") + String(": ") + String(m_data) + String(" (") + String("degrees Celsius") + String(")") + String(" | ");
-}
