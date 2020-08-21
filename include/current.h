0254deaf4785570ccc3399ddb2002d7e3c2793de
RyanL17 <ryanL071301@gmail.com>
2020-08-21 13:29:57 -0700

    adjusted current.h to make use of inheritance (updated with changes)

--- include/current.h
+++ include/current.h
@@ -14,13 +14,11 @@
 class current: public farmSensor
 {
 private:
-    float readRaw() override; //returns currentage of source (NOT THE DIVIDER) in decimal value
-protected:
- 
-public:
-    //constants declarations (cannot declare static const floats in .h files, need .cpp --> 3.9 defined in readRaw())
+     //constants declarations (cannot declare static const floats in .h files, need .cpp --> 3.9 defined in readRaw())
     static const int adc_res = 4095; //ESP32 ADC resolution is 12 bits
- 
+protected:
+    float readRaw() override; //returns currentage of source (NOT THE DIVIDER) in decimal value
+public: 
     //functions
     current(int pin, String name, String unit, char shortcut);
     void initialize() override;
