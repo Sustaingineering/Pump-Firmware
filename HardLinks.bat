4114f8706cf3205a06972f0fc65b9fd1d719cb5b
tarek20501 <39224871+tarek20501@users.noreply.github.com>
2020-08-30 12:59:34 -0700

    ESP32 remove common files

---
+++ HardLinks.bat
@@ -0,0 +1,31 @@
+mklink /H ESP32\src\main.cpp Common\src\main.cpp
+mklink /H Particle\src\main.ino Common\src\main.cpp
+
+mklink /H ESP32\include\counter.h Common\include\counter.h
+mklink /H Particle\src\counter.h Common\include\counter.h
+
+mklink /H ESP32\include\current.h Common\include\current.h
+mklink /H Particle\src\current.h Common\include\current.h
+
+mklink /H ESP32\include\farmSensor.h Common\include\farmSensor.h
+mklink /H Particle\src\farmSensor.h Common\include\farmSensor.h
+
+mklink /H ESP32\include\Flow.h Common\include\Flow.h
+mklink /H Particle\src\Flow.h Common\include\Flow.h
+
+mklink /H ESP32\include\LoRaTransceiver.h Common\include\LoRaTransceiver.h
+mklink /H Particle\src\LoRaTransceiver.h Common\include\LoRaTransceiver.h
+
+mklink /H ESP32\include\SdCard.h Common\include\SdCard.h
+mklink /H Particle\src\SdCard.h Common\include\SdCard.h
+
+mklink /H ESP32\include\temp.h Common\include\temp.h
+mklink /H Particle\src\temp.h Common\include\temp.h
+
+mklink /H ESP32\include\volt.h Common\include\volt.h
+mklink /H Particle\src\volt.h Common\include\volt.h
+
+mklink /H ESP32\include\watch.h Common\include\watch.h
+mklink /H Particle\src\watch.h Common\include\watch.h
+
+pause
