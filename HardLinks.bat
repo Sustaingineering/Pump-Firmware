2bee0fce9ca4ac3b0b7ca6c872714212c197cca6
tarek20501 <39224871+tarek20501@users.noreply.github.com>
2020-08-30 20:26:21 -0700

    Use FOR loops in HardLinks.bat

--- HardLinks.bat
+++ HardLinks.bat
@@ -1,31 +1,11 @@
-mklink /H ESP32\src\main.cpp Common\src\main.cpp
-mklink /H Particle\src\main.ino Common\src\main.cpp
+rm Particle\src\main.cpp
 
-mklink /H ESP32\include\counter.h Common\include\counter.h
-mklink /H Particle\src\counter.h Common\include\counter.h
+for /f %%F in ('ls Common\include') do mklink /H ESP32\include\%%F Common\include\%%F
+for /f %%F in ('ls Common\include') do mklink /H Particle\src\%%F Common\include\%%F
 
-mklink /H ESP32\include\current.h Common\include\current.h
-mklink /H Particle\src\current.h Common\include\current.h
+for /f %%F in ('ls Common\src') do mklink /H ESP32\src\%%F Common\src\%%F
+for /f %%F in ('ls Common\src') do mklink /H Particle\src\%%F Common\src\%%F
 
-mklink /H ESP32\include\farmSensor.h Common\include\farmSensor.h
-mklink /H Particle\src\farmSensor.h Common\include\farmSensor.h
-
-mklink /H ESP32\include\Flow.h Common\include\Flow.h
-mklink /H Particle\src\Flow.h Common\include\Flow.h
-
-mklink /H ESP32\include\LoRaTransceiver.h Common\include\LoRaTransceiver.h
-mklink /H Particle\src\LoRaTransceiver.h Common\include\LoRaTransceiver.h
-
-mklink /H ESP32\include\SdCard.h Common\include\SdCard.h
-mklink /H Particle\src\SdCard.h Common\include\SdCard.h
-
-mklink /H ESP32\include\temp.h Common\include\temp.h
-mklink /H Particle\src\temp.h Common\include\temp.h
-
-mklink /H ESP32\include\volt.h Common\include\volt.h
-mklink /H Particle\src\volt.h Common\include\volt.h
-
-mklink /H ESP32\include\watch.h Common\include\watch.h
-mklink /H Particle\src\watch.h Common\include\watch.h
+mv Particle\src\main.cpp Particle\src\main.ino
 
 pause
