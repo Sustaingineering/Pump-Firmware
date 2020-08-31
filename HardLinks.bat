4c09b573e19baf867765ac4d59c8f3d20a86b3b1
tarek20501 <39224871+tarek20501@users.noreply.github.com>
2020-08-31 15:16:54 -0700

    Refine HardLinks.bat

--- HardLinks.bat
+++ HardLinks.bat
@@ -1,15 +1,27 @@
-for /f %%F in ('ls Common\include') do rm ESP32\include\%%F
-for /f %%F in ('ls Common\include') do rm Particle\src\%%F
+@echo off
 
-for /f %%F in ('ls Common\src') do rm ESP32\src\%%F
-for /f %%F in ('ls Common\src') do rm Particle\src\%%F
+echo Removing Particle\src\main.ino
+rm Particle\src\main.ino
 
-for /f %%F in ('ls Common\include') do mklink /H ESP32\include\%%F Common\include\%%F
-for /f %%F in ('ls Common\include') do mklink /H Particle\src\%%F Common\include\%%F
+for /f %%F in ('ls Common\include') do (
+    echo Removing ESP32\include\%%F
+    rm ESP32\include\%%F
+    echo Removing Particle\src\%%F
+    rm Particle\src\%%F
+    mklink /H ESP32\include\%%F Common\include\%%F
+    mklink /H Particle\src\%%F Common\include\%%F
+)
 
-for /f %%F in ('ls Common\src') do mklink /H ESP32\src\%%F Common\src\%%F
-for /f %%F in ('ls Common\src') do mklink /H Particle\src\%%F Common\src\%%F
+for /f %%F in ('ls Common\src') do (
+    echo Removing ESP32\src\%%F
+    rm ESP32\src\%%F
+    echo Removing Particle\src\%%F
+    rm Particle\src\%%F
+    mklink /H ESP32\src\%%F Common\src\%%F
+    mklink /H Particle\src\%%F Common\src\%%F
+)
 
+echo Renaming Particle\src\main.cpp to Particle\src\main.ino 
 mv Particle\src\main.cpp Particle\src\main.ino
 
 rm Particle\src\temp.cpp
