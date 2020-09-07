ffc4880fde6f9f69d9b30e7d868ade070fb486b5
Tarek Ayoubi <tarek20501@hotmail.com>
2020-09-07 00:23:32 -0700

    Common: use Windows cmd binaries in create_hard_links.bat

--- create_hard_links.bat
+++ create_hard_links.bat
@@ -1,29 +1,33 @@
 @echo off
 
 echo Removing Particle\src\main.ino
-rm Particle\src\main.ino
+del Particle\src\main.ino
 
-for /f %%F in ('ls Common\include') do (
+for /f %%F in ('dir /b Common\include') do (
     echo Removing ESP32\include\%%F
-    rm ESP32\include\%%F
+    del ESP32\include\%%F
+
     echo Removing Particle\src\%%F
-    rm Particle\src\%%F
+    del Particle\src\%%F
+
     mklink /H ESP32\include\%%F Common\include\%%F
     mklink /H Particle\src\%%F Common\include\%%F
 )
 
-for /f %%F in ('ls Common\src') do (
+for /f %%F in ('dir /b Common\src') do (
     echo Removing ESP32\src\%%F
-    rm ESP32\src\%%F
+    del ESP32\src\%%F
+
     echo Removing Particle\src\%%F
-    rm Particle\src\%%F
+    del Particle\src\%%F
+    
     mklink /H ESP32\src\%%F Common\src\%%F
     mklink /H Particle\src\%%F Common\src\%%F
 )
 
 echo Renaming Particle\src\main.cpp to Particle\src\main.ino 
-mv Particle\src\main.cpp Particle\src\main.ino
+move Particle\src\main.cpp Particle\src\main.ino
 
-rm Particle\src\LoRaTransceiver.cpp
+del Particle\src\LoRaTransceiver.cpp
 
 pause
