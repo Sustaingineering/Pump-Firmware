a55fca61f2b02c6b7feee791a07008d21831b967
Ryan <ryanl071301@gmail.com>
2020-10-31 13:19:16 -0700

    #39 formatted main.cpp according to coding standards. Have not changed function names yet.

--- Common/src/main.cpp
+++ Common/src/main.cpp
@@ -22,193 +22,186 @@
 String message;
 
 #ifdef electron
-Gsm gsm;
+    Gsm gsm;
 #endif
 
 #if CURRENT
-current hall_effect(CURRENT_PIN, "Current", "Amps", 'i', MAX_V);
+    current hall_effect(CURRENT_PIN, "Current", "Amps", 'i', MAX_V);
 #endif
 
 #if VOLTAGE
-volt volt_divider(VOLT_PIN, analog, "Voltage", "Volt", 'v', 25000, 1000000, MAX_V);
+    volt volt_divider(VOLT_PIN, analog, "Voltage", "Volt", 'v', 25000, 1000000, MAX_V);
 #endif
 
 #if TEMP
-temp thermocouple(TEMP_PIN, digital, "Temperature", "Celsius", 't'); //pretty slow response and depends greatly on the surface temperature of the thermocouple tip
+    temp thermocouple(TEMP_PIN, digital, "Temperature", "Celsius", 't'); //pretty slow response and depends greatly on the surface temperature of the thermocouple tip
 #endif
 
 #if FLOW
-flow waterflow(FLOW_PIN,"WaterFlow", "L/min", 'f');
+    flow waterflow(FLOW_PIN,"WaterFlow", "L/min", 'f');
 #endif
 
 #if ERTC
-RealTimeClock rtc;
+    RealTimeClock rtc(false);
 #endif
 
 #if SDCARD
-SdCard memory(SDCARD_SELECT_PIN);
+    SdCard memory(SDCARD_SELECT_PIN);
 #endif
 
 #if LORA
-bool LoRaStatus;
-packet packets[NUMBER_OF_PACKETS];
+    bool LoRaStatus;
+    packet packets[NUMBER_OF_PACKETS];
 #if GSM
-LoRaTransceiver requester(LORA_SELECT_PIN, LORA_RST_PIN, LORA_DIO0_PIN, LORA_SECRET_WORD);
+    LoRaTransceiver requester(LORA_SELECT_PIN, LORA_RST_PIN, LORA_DIO0_PIN, LORA_SECRET_WORD);
 #else
-LoRaTransceiver responder(LORA_SELECT_PIN, LORA_RST_PIN, LORA_DIO0_PIN, LORA_SECRET_WORD, PUMP_ID);
+    LoRaTransceiver responder(LORA_SELECT_PIN, LORA_RST_PIN, LORA_DIO0_PIN, LORA_SECRET_WORD, PUMP_ID);
 #endif //GSM
 #endif //LORA
 
 #if COUNTERS
-  #define NUM_COUNTERS 6
-  counter *counterArray[NUM_COUNTERS];
-  void createCounters()
-  {
-    char letter = 'a';
-
-    for (int j = 0; j < NUM_COUNTERS; j++)
+    #define NUM_COUNTERS 6
+    counter *counterArray[NUM_COUNTERS];
+    void createCounters()
     {
-      String name = "Counter";
-      name += j;
-      counterArray[j] = new counter(j, name, "T", letter);
-      letter += 1;
+        char letter = 'a';
+
+        for (int j = 0; j < NUM_COUNTERS; j++)
+        {
+        String name = "Counter";
+        name += j;
+        counterArray[j] = new counter(j, name, "T", letter);
+        letter += 1;
+        }
     }
-  }
 #endif
 
 void setup()
 {
-  pinMode(BUILTIN_LED, OUTPUT);
-  Serial.begin(115200);
-  Serial.println("\nHello Sustaingineering!\n");
+    pinMode(BUILTIN_LED, OUTPUT);
+    Serial.begin(115200);
+    Serial.println("\nHello Sustaingineering!\n");
 
-#ifdef electron
-  gsm.initialize();
-#endif
+    #if CURRENT
+        Serial.println("Initializing Current Sensor...");
+        hall_effect.initialize();
+        Serial.println("Current Sensor Initialized.\n");
+    #endif
+
+    #if VOLTAGE
+        Serial.println("Initializing Voltage Sensor...");
+        volt_divider.initialize();
+        Serial.println("Voltage Sensor Initialized.\n");
+    #endif
   
-#if CURRENT
-  Serial.println("Initializing Current Sensor...");
-  hall_effect.initialize();
-  Serial.println("Current Sensor Initialized.\n");
-#endif
+    #if TEMP
+        Serial.println("Initializing Thermocouple...");
+        thermocouple.initialize();
+        Serial.println("Thermocouple Initialized.\n");
+    #endif
 
-#if VOLTAGE
-  Serial.println("Initializing Voltage Sensor...");
-  volt_divider.initialize();
-  Serial.println("Voltage Sensor Initialized.\n");
-#endif
+    #if FLOW
+        Serial.println("Initializing Waterflow...");
+        waterflow.initialize();
+        Serial.println("WaterFlow Initialized.\n");
+    #endif
+
+    #if ERTC
+        Serial.println("Initializing RTC...");
+        rtc.initialize();
+        Serial.println("RTC Initialized.\n");
+    #endif
+
+    #if SDCARD
+        Serial.println("Initializing MicroSD Card...");
+        memory.initialize();
+        Serial.println("MicroSD Card Initialized.\n");
+    #endif
+
+    #if LORA
+        Serial.println("Initializing LoRa...");
+    #if GSM
+        requester.initialize();
+    #else
+        responder.initialize();
+    #endif // GSM
+        Serial.println("LoRa Initialized.\n");
+    #endif // LORA
+
+    //Sensors Initializers go here.
   
-#if TEMP
-  Serial.println("Initializing Thermocouple...");
-  thermocouple.initialize();
-  Serial.println("Thermocouple Initialized.\n");
-#endif
-
-#if FLOW
-  Serial.println("Initializing Waterflow...");
-  waterflow.initialize();
-  Serial.println("WaterFlow Initialized.\n");
-#endif
-
-#if ERTC
-  Serial.println("Initializing RTC...");
-  rtc.initialize(1604177282); //Oct 31, 2020 - 1:48
-  Serial.println("RTC Initialized.\n");
-#endif
-
-#if SDCARD
-  Serial.println("Initializing MicroSD Card...");
-  memory.initialize();
-  Serial.println("MicroSD Card Initialized.\n");
-#endif
-
-#if LORA
-  Serial.println("Initializing LoRa...");
-#if GSM
-  requester.initialize();
-#else
-  responder.initialize();
-#endif // GSM
-  Serial.println("LoRa Initialized.\n");
-#endif // LORA
-
-#if COUNTERS
-  createCounters();
-#endif
-
-  //Sensors Initializers go here.
-
-  Serial.println("Setup Done!\n");
+    Serial.println("Setup Done!\n");
 }
 
 
 void loop()
 {
-  message = "";
-  //Sampling Sensors
-#if COUNTERS
-  for (int i = 0; i < NUM_COUNTERS; i++)
-    message += counterArray[i]->read();
-#endif
+    message = "";
+    //Sampling Sensors
+    #if COUNTERS
+        for (int i = 0; i < NUM_COUNTERS; i++)
+        message += counterArray[i]->read();
+        
+    #if CURRENT
+        message += hall_effect.read();
+    #endif
 
-#if CURRENT
-  message += hall_effect.read();
-#endif
+    #if VOLTAGE
+        message += volt_divider.read();
+    #endif
 
-#if VOLTAGE
-  message += volt_divider.read();
-#endif
+    #if TEMP
+        message += thermocouple.read();
+    #endif
 
-#if TEMP
-  message += thermocouple.read();
-#endif
+    #if FLOW
+        message += waterflow.read();
+    #endif
 
-#if FLOW
-  message += waterflow.read();
-#endif
+    #if ERTC
+        message += rtc.getTimeStamp();
+    #endif
 
-#if ERTC
-  message += rtc.getTimeStamp();
-#endif
-  message += String("\n");
-  Serial.print(message);
-#if SDCARD
-  //Writing on Sd Card
-  memory.appendFile(("/" + rtc.getDate() + ".txt").c_str(), message.c_str());
-#endif
+    message += String("\n");
+    Serial.print(message);
+  
+    #if SDCARD
+        //Writing on Sd Card
+        memory.appendFile(("/" + rtc.getDate() + ".txt").c_str(), message.c_str());
+    #endif
 
-#if LORA
-#ifdef electron 
-  // make requests
-  requester.request(0, packets, NUMBER_OF_PACKETS);
-  Serial.print("received = [");
-  for (int i = 0; i < NUMBER_OF_PACKETS; i++)
-  {
-    Serial.print(packets[i].type);
-    Serial.print(packets[i].data);
-    if (i != (NUMBER_OF_PACKETS - 1))
-      Serial.print(", ");
-  }
-  Serial.print("]");
-  delay(100);
-#else //electron
-  //Responding to a request from LoRa
-  packets[0] = counter1.pack();
-  packets[1] = counter2.pack();
-  packets[2] = counter3.pack();
-  packets[3] = counter4.pack();
-  packets[4] = counter5.pack();
-  packets[5] = counter6.pack();
-  LoRaStatus = responder.respond(packets, NUMBER_OF_PACKETS);
-#endif // electron
-#else //LORA
-  delay(1000);
-#endif //LORA
+    #if LORA
+    #ifdef electron 
+        // make requests
+        requester.request(0, packets, NUMBER_OF_PACKETS);
+        Serial.print("received = [");
+        for (int i = 0; i < NUMBER_OF_PACKETS; i++)
+        {
+            Serial.print(packets[i].type);
+            Serial.print(packets[i].data);
+        if (i != (NUMBER_OF_PACKETS - 1))
+            Serial.print(", ");
+        }
+        Serial.print("]");
+        delay(100);
+    #else //electron
+        //Responding to a request from LoRa
+        packets[0] = counter1.pack();
+        packets[1] = counter2.pack();
+        packets[2] = counter3.pack();
+        packets[3] = counter4.pack();
+        packets[4] = counter5.pack();
+        packets[5] = counter6.pack();
+        LoRaStatus = responder.respond(packets, NUMBER_OF_PACKETS);
+    #endif // electron
+    #else //LORA
+        delay(1000);
+    #endif //LORA
 
-#ifdef electron
-  gsm.Publish(String(PUMP_ID), message);
-#endif
+    #ifdef electron
+        gsm.Publish(String(PUMP_ID), message);
+    #endif
 
-  //restarter.takeAction(LoRaStatus);
-  Serial.println();
+        //restarter.takeAction(LoRaStatus);
+        Serial.println();
 }
