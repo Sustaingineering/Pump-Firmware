6c935ba748e4c7674db23fe0b925dd2badc60d2c
Tarek Ayoubi <tarek20501@hotmail.com>
2020-11-07 14:15:26 -0800

    Particle: Add PinConfig diagram (#50)

--- Particle/src/PinConfig.h
+++ Particle/src/PinConfig.h
@@ -1,4 +1,32 @@
 #pragma once
+
+/*
+                    _________________________
+                   |VIN                   3V3|o
+                   |GND                   RST|o
+                   |Tx                   VBAT|o
+                  o|Rx                    GND|o
+                  o|WKP    [Particle]      D7|o
+                  o|DAC                    D6|o
+       SD(MOSI)-> *|A5:SPI:MOSI            D5|o
+       SD(MISO)-> *|A4:SPI:MISO            D4|o
+        SD(SCK)-> *|A3:SPI(SCK)            D3|o
+         SD(SS)-> *|A2:SPI(SS)             D2|o
+                  o|A1                     D1|o
+                  o|A0                     D0|o
+                  o|B5                     C5|o
+    Temperature-> o|B4                     C4|o
+        Current-> o|B3                     C3|o
+        Voltage-> *|B2                     C2|o
+                  o|B1                     C1|o
+                  o|B0____________________ C0|o
+
+(o): unused.
+(*): used.
+(x): cannot be used.
+(?): WTF.
+*/
+
 #define electron
 #define BUILTIN_LED D7
 #define ERTC      1
