e5962e16820351c73448d4f978b977f7a1df98fc
ShreyansK2000 <gshreyansk4@gmail.com>
2020-11-07 14:23:38 -0800

    Added period calculation based on message length

--- Particle/src/Gsm.cpp
+++ Particle/src/Gsm.cpp
@@ -1,12 +1,59 @@
 #include "Gsm.h"
 
+
+void Gsm::initialize()
+{
+    m_maxData0Bytes = MAX_DATA_0;
+    m_publishPeriod = DEFAULT_PERIOD;
+}
+
 void Gsm::Publish(String pumpId, String message)
 {
     int now = (int)Time.now();
-    if (now - m_lastTimePublished >= 10)
+    if (now - m_lastTimePublished >= m_publishPeriod)
     {
+        updatePublishInfo_(); 
+
         Particle.publish(pumpId.c_str(), message.c_str(), PRIVATE);
         m_lastTimePublished = now;
         Serial.println("Particle Published!");
+
+        if (m_numPublish == 2)
+        {
+            computePublishPeriod_();
+        }
+    } 
+}
+
+void Gsm::computePublishPeriod_()
+{
+    delay(5000);
+    int messageLength = getTotalDataUsage_() - m_currentUsageBytes;
+    Serial.printlnf("Message Length: %d bytes", messageLength);
+
+    int numMessagesPerMonth = (MAX_DATA_LIMIT_BYTES - (m_maxData0Bytes * NUM_DAYS)) / messageLength;
+
+    m_publishPeriod = NUM_DAYS * NUM_HOURS * SECONDS_IN_HOUR / numMessagesPerMonth;
+    Serial.printlnf("Publish Period: %d seconds", m_publishPeriod);
+}
+
+void Gsm::updatePublishInfo_()
+{
+    m_numPublish++;
+    m_currentUsageBytes = getTotalDataUsage_();
+    Serial.printlnf("Current Total Data usage: %d bytes", m_currentUsageBytes);
+}
+
+int Gsm::getTotalDataUsage_()
+{
+    CellularData data;
+    Cellular.getDataUsage(data);
+    if (!Cellular.getDataUsage(data)) {
+        Serial.println("Error! Not able to get data.");
+        return 0;
+    }
+    else {
+        Serial.println(data);
+        return data.tx_total + data.rx_total;
     }
 }
--- Particle/src/Gsm.h
+++ Particle/src/Gsm.h
@@ -7,10 +7,53 @@
 #pragma once
 #include <Arduino.h>
 
+#define MAX_DATA_0 6400
+#define MAX_DATA_LIMIT_MB 3
+#define MAX_DATA_LIMIT_BYTES (MAX_DATA_LIMIT_MB * 1024 * 1024)
+#define NUM_DAYS 31
+#define NUM_HOURS 24
+#define SECONDS_IN_HOUR 3600
+#define DEFAULT_PERIOD 10
+
 class Gsm
 {
 private:
     int m_lastTimePublished = 0;
+    int m_publishPeriod = 0;
+    int m_numPublish = 0;
+    int m_currentUsageBytes = 0;
+    int m_maxData0Bytes = 0;
+
+    /**
+     * @brief Computes the period at which the GSM module will publish messages
+     * based on the maximum data limit and test-derived parameters.
+     */
+    void computePublishPeriod_();
+
+    /**
+     * @brief Get the total data usage (sent + received).
+     * 
+     * @return int Number of bytes sent and received.
+     */
+    int getTotalDataUsage_();
+
+    /**
+     * @brief Update private members with number of publishes and current total usage.
+     */
+    void updatePublishInfo_();
 public:
+
+    /**
+     * @brief Currently only initializes private members with defined macros. 
+     * Stub for future functionality. 
+     */
+    void initialize();
+
+    /**
+     * @brief Calls the Particle publish method based on time and data restrictions
+     * 
+     * @param String pumpId: Stringified integer representing the pump ID
+     * @param String message: String containing data to be published by the Particle
+     */
     void Publish(String pumpId, String message);
 };
