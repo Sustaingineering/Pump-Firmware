97633e29669bf1fc426f8a0838341ba207c6e416
ShreyansK2000 <gshreyansk4@gmail.com>
2021-03-06 12:16:49 -0800

    Minor changes to flow, codebase compiles on Boron (Eli tested)

--- Common/src/Flow.cpp
+++ Common/src/Flow.cpp
@@ -3,34 +3,33 @@
 namespace Water
 {
     unsigned int numberOfFanTops;
-    void numberOfFanTopsCounter()
+    void countFlowInterrupts()
     {
         numberOfFanTops++;
     }
 }
 
-Flow::Flow(int pin,  String name, String unit, char shortcut):FarmSensor(pin,name,unit,shortcut)
-{
-}
+Flow::Flow(int pin,  String name, String unit, char shortcut):
+        FarmSensor(pin,name,unit,shortcut) {}
  
 void Flow::initialize()
 {
     pinMode(m_pin, INPUT); //initializes digital pin as an input
-    attachInterrupt(digitalPinToInterrupt(m_pin), Water::numberOfFanTopsCounter, RISING); //and the interrupt is attached
+    attachInterrupt(digitalPinToInterrupt(m_pin), Water::countFlowInterrupts, RISING); //and the interrupt is attached
 }
 
 float Flow::readRaw()
 {
     Water::numberOfFanTops = 0;   
+
     sei(); //Enables interrupts
     delay (1000); //Wait 1 second
     cli(); //Disable interrupts
+
     //calculations subtract number of times called outside of interupt
-    int flowRate = (Water::numberOfFanTops * 60 / 5.5); //(Pulse frequency x 60) / 5.5Q, = flow rate in L/min
-    Serial.print (flowRate,DEC); //Prints flow rate calculated above
-    Serial.print (" L/min\r\t"); //Prints "L/hour" and returns a new line
-    Serial.print ("numberOfFanTops:");
-    Serial.print (Water::numberOfFanTops, DEC);
-    Serial.print ("\n");
+    int flowRate = (Water::numberOfFanTops * 60 / FLOW_RATE_FACTOR); //(Pulse frequency x 60) / 5.5Q, = flow rate in L/min
+    Serial.printf("Flow Rate: %d L/min\n",flowRate); //Prints flow rate calculated above
+    Serial.printf("numberOfFanTops: %d \n", Water::numberOfFanTops);
+    Serial.print("\n");
     return flowRate;
 }
--- Particle/src/PinConfig.h
+++ Particle/src/PinConfig.h
@@ -52,6 +52,7 @@
 #define VOLT_PIN     A0
 #define CURRENT_PIN  A1
 #define TEMP_PIN     A2
+#define FLOW_PIN     A3
 #define SDCARD_SELECT_PIN  SCK
 #define MAX_V 3.3
 #endif
