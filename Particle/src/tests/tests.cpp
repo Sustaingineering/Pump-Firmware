e1e20e925d1a396f69e3d4f59c9cccb3a1e34cae
ShreyansK2000 <gshreyansk4@gmail.com>
2021-05-15 11:47:39 -0700

    #115 add extern links to classes required for testing

--- Particle/src/tests/tests.cpp
+++ Particle/src/tests/tests.cpp
@@ -1,8 +1,16 @@
-#include "PinConfig.h"
-#include <Arduino.h>
 #include "tests.h"
 
 void tests() {
     Serial.println("Hello Unit Tests");
+    // Test that extern objects are available
+    Serial.printlnf("%s", rtc.getTimeStamp().c_str());
     while(1);
+}
+
+void printTestStart(String tag) {
+    Serial.printlnf("----- START OF %s TEST -----", tag.c_str());
+}
+
+void printTestEnd(String tag) {
+    Serial.printlnf("------ END OF %s TEST ------", tag.c_str());
 }
--- Particle/src/tests/tests.h
+++ Particle/src/tests/tests.h
@@ -1,1 +1,41 @@
-void tests();
+#include "PinConfig.h"
+#include <Arduino.h>
+
+#if EN_GSM
+#include "Gsm.h"
+extern Gsm gsm;
+#endif
+
+#if CURRENT
+#include "Current.h"
+extern Current hall_effect;
+#endif
+
+#if VOLTAGE
+#include "Voltage.h"
+extern Voltage volt_divider;
+#endif
+
+#if TEMPERATURE
+#include "Temperature.h"
+extern Temperature thermocouple; //pretty slow response and depends greatly on the surface temperature of the thermocouple tip
+#endif
+
+#if FLOW
+#include "Flow.h"
+extern Flow waterflow;
+#endif
+
+#if ERTC
+#include "RealTimeClock.h"
+extern RealTimeClock rtc;
+#endif
+
+#if SDCARD
+#include "SdCard.h"
+extern SdCard memory;
+#endif
+
+void tests(); 
+void printTestStart(String tag);
+void printTestEnd(String tag);
