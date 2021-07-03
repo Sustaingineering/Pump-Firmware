57ab076f6c4143d75d0ef8fe6f3c890a304b7b26
jack5661 <jack5661@hotmail.ca>
2021-07-03 11:22:06 -0700

    Add extra space to fail test msg

--- Particle/src/tests/tests.h
+++ Particle/src/tests/tests.h
@@ -2,12 +2,11 @@
 #include <Arduino.h>   
 #include <string.h>
 
-#define TEST_FAILED Serial.println("!!!!! TEST FAILED !!!!!"); return
+#define TEST_FAILED Serial.printlnf("!!!!! TEST FAILED !!!!!\n"); return
 #define TEST_FAIL_MESSAGE(msg) Serial.printf("TEST FAILED: %s\n", msg); return
 
 #define TEST_ASSERT_NULL(ptr) if (ptr != NULL) TEST_FAIL_MESSAGE("Pointer not NULL")
 #define TEST_ASSERT_EQUAL_STRING(str1, str2) if (strcmp(str1, str2)) TEST_FAIL_MESSAGE("Strings not equal")
-
 
 #if EN_GSM
 #include "Gsm.h"
