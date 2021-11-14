a90c81570f91bdd58ca82ea008a7752ccc523dc9
Tarek Ayoubi <tarek20501@hotmail.com>
2021-11-14 15:34:08 -0800

    Added main.h to reduce clutter in main.cpp

---
+++ Common/include/main.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <Arduino.h>
+#include "PinConfig.h"
+#include "RealTimeClock.h"
+#include "SdCard.h"
+#include "FarmSensor.h"
+#include "Voltage.h"
+#include "Current.h"
+#include "Temperature.h"
+#include "Flow.h"
+#ifdef PARTICLE_H
+#include "Gsm.h"
+#endif
+
+int pumpId = 0;         // FIXME: do it in persistent data class
+
+#ifdef PARTICLE_H
+Gsm gsm(GSM_SWITCH);
+#endif
+
+//Global Objects
+String message;
+
+RealTimeClock rtc;
+
+Current hall_effect(CURRENT_SWITCH, CURRENT_PIN, MAX_V);
+
+Voltage volt_divider(VOLTAGE_SWITCH, VOLT_PIN, 25000, 1000000, MAX_V);
+
+Temperature thermocouple(TEMPERATURE_SWITCH, TEMP_PIN); //pretty slow response and depends greatly on the surface temperature of the thermocouple tip
+
+Flow waterflow(FLOW_SWITCH, FLOW_PIN);
+
+SdCard memory(SDCARD_SWITCH, SDCARD_SELECT_PIN);
+// FIXME: do it in persistent data class
+void pumpIdInit()
+{
+  char *idBuf = memory.readFile("/pump-id.txt");
+  if (idBuf != NULL)
+  {
+    pumpId = strtol(idBuf, NULL, 10);
+    free(idBuf);
+    Serial.printf("PumpID is: %d\n", pumpId);
+  }
+}
